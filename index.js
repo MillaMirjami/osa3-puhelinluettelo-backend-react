51a5ee9ea56411ad5b3699628e7ec5f7d3de98bc
MillaMirjami <milla.sukki@gmail.com>
2025-03-10 12:37:43 +0200

    uibuild

--- index.js
+++ index.js
@@ -34,6 +34,7 @@ morgan.token('body', (req) => {
 // Start using middlewares
 app.use(cors())
 app.use(express.json())
+app.use(express.static('dist'))
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
 
 app.get('/api/persons', (request, response) => {
