4e265b1ece426111f75e43eb4ffb6e263faa10cc
MillaMirjami <milla.sukki@gmail.com>
2025-03-19 14:58:11 +0200

    mongoose added

--- Models/person.js
+++ Models/person.js
@@ -11,11 +11,21 @@ const url = `mongodb+srv://fullstack:${password}@cluster0.5i94f.mongodb.net/Phon
 
 mongoose.set('strictQuery', false)
 mongoose.connect(url)
+.then(result => console.log('Connected to MongoDB'))
+.catch(error => console.log('Error connecting to db', error.message))
 
 const personSchema = new mongoose.Schema({
     name: String,
     number: String
 })
+
+personSchema.set('toJSON', {
+    transform: (document, returnedObject) => {
+      returnedObject.id = returnedObject._id.toString()
+      delete returnedObject._id
+      delete returnedObject.__v
+    }
+  })
 
 const Person = mongoose.model('Person', personSchema)
 
@@ -27,7 +37,6 @@ if(process.argv.length > 3) {
 
     person.save().then(result => {
         console.log(`added ${result.name} number ${result.number} to phonebook`)
-        mongoose.connection.close()
     })
 }
 
@@ -38,6 +47,7 @@ else if (process.argv.length === 3) {
         console.log('Phonebook:')
         persons.forEach(person => { console.log(`${person.name} ${person.number}`)
         })
-    mongoose.connection.close()
     })
 }
+
+module.exports = mongoose.model('Person', personSchema)
--- index.js
+++ index.js
@@ -1,29 +1,7 @@
 const express = require('express')
 const morgan = require('morgan')
 const app = express()
-
-let persons = [
-    {
-        id: "1",
-        name: "Arto Hellas",
-        number: "040-123456"
-    },
-    {
-        id: "2",
-        name: "Ada Lovelance",
-        number: "39-44-5323523"
-    },
-    {
-        id: "3",
-        name: "Dan Abramov",
-        number: "12-43-234345"
-    },
-    {
-        id: "4",
-        name: "Mary Poppendieck",
-        number: "39-23-6423122"
-    }
-]
+const Person = require('./Models/person')
 
 // Create your own logger token
 morgan.token('body', (req) => {
@@ -36,72 +14,40 @@ app.use(express.static('dist'))
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
 
 app.get('/api/persons', (request, response) => {
-    response.send(persons)
+    Person.find({}).then(persons => response.json(persons))
 })
 
 app.get('/api/persons/:id', (request, response) => {
-    const id = request.params.id
-    const person = persons.find(p => p.id === id)
-
-    if(person) {
-        response.send(person)
-    } else {
-        response.status(404).send({error: "person not found"})
-    }
+    Person.findById(request.params.id).then(person => response.json(person))
 })
 
-const generateId = () => {
-
-    if(persons.length > 0) {
-        const currentIds = persons.map(p => Number(p.id))
-        let randomId
-        do {
-            randomId = Math.floor(Math.random() * (1000000 - 5 + 1)) + 5
-        }while(currentIds.includes(randomId))
-            return String(randomId)
-    } else {
-        return "1"
-    }
-
-}
 
 
 app.post('/api/persons', (request, response) => {
     const body = request.body
-
+    
     if(!body.name || !body.number) {
         return response.status(400).json({error: "name or number missing"})
     }
-    if(persons.find(p => p.name.toLowerCase() === body.name.toLowerCase())) {
-        return response.status(400).json({error: "this name already exists"})
-    }
-
-    const person = {
-        id: generateId(),
+    
+    const person = new Person({
         name: body.name,
         number: body.number
-    }
-
-    persons = persons.concat(person)
-    response.json(person)
+    })
     
-
+    person.save().then(savedPerson => response.json(savedPerson))
 })
 
 app.put('/api/persons/:id', (request, response) => {
-    const body = request.body
-    response.send(body)
+    Person.findByIdAndUpdate(request.params.id, request.body).then(updatedPerson => response.send(updatedPerson))
 })
 
 app.delete('/api/persons/:id', (request, response) => {
-    const id = request.params.id
-    persons = persons.filter(p => p.id !== id)
-
-    response.status(204).end()
+    Person.findByIdAndDelete(request.params.id).then(deletedPerson => response.status(204).send('deletion successful'))
 })
 
 app.get('/info', (request, response) => {
-    const personsLength = persons.length
+    const personsLength = Person.countDocuments({}).number
     const currentDate= new Date()
     response.send(`<p>Phonebook has info for ${personsLength} people</p><p>${currentDate}</p>`)
 })
@@ -111,3 +57,17 @@ const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
 })
+
+// const generateId = () => {
+
+//     if(persons.length > 0) {
+//         const currentIds = persons.map(p => Number(p.id))
+//         let randomId
+//         do {
+//             randomId = Math.floor(Math.random() * (1000000 - 5 + 1)) + 5
+//         }while(currentIds.includes(randomId))
+//             return String(randomId)
+//     } else {
+//         return "1"
+//     }
+// }
